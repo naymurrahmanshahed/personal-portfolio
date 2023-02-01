a42b35110af8db1fc168675e3d30d37998fb22a9
naymurrahmanshahed <shahed285@outlook.com>
2023-02-01 14:28:37 +0600

    hero done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./Home";
-
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
+      <div className="noise"></div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,27 @@
+import { useRef } from "react";
+import { useHoverEffect } from "../hooks/useHoverEffect";
+
+const data = {
+  img1: "https://images.pexels.com/photos/13452647/pexels-photo-13452647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  img2: "https://images.pexels.com/photos/3190334/pexels-photo-3190334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+};
+
+const Hero = () => {
+  const heroImgRef = useRef(null);
+  useHoverEffect(heroImgRef, data.img1, data.img2);
+  return (
+    <div className="hero container mx-auto mt-20  flex  justify-center items-center overflow-hidden">
+      <div className="hero-img" ref={heroImgRef}></div>
+      <div className="shutter shutter-left overflow-hidden ">
+        <h1>front-end</h1>
+      </div>
+      <div className="shutter shutter-right overflow-hidden">
+        <h1>Developer</h1>
+      </div>
+      <div className="circle-left"></div>
+      <div className="circle-right"></div>
+    </div>
+  );
+};
+
+export default Hero;
